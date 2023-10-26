7132bc52d25dc0262563fc06f49fc4e1f8c026d3
HarshPrajapati2003 <hrsdp2683@gmail.com>
2023-10-26 18:19:04 +0530

    Production level changes.

--- backend/server.js
+++ backend/server.js
@@ -6,21 +6,32 @@ const userRoutes = require('./routes/userRoutes')
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require('./middleware/errorMiddleware')
+const path = require('path')
 
 dotenv.config();
 connectDB();
 const app = express()
 app.use(express.json())  //to accept json data
 
-app.get('/', (req,res) => {
-    res.send('api is running')
-})
-
 app.use('/api/user', userRoutes)
 app.use('/api/chat', chatRoutes)
 app.use("/api/message", messageRoutes);
 
-app.use(notFound)
+// ======================= deployment ============================
+
+const __dirname1 = path.resolve()
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname1, "/frontend/build")))
+     app.get("*", (req, res) => {
+       res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"))
+     });
+} else {
+    app.get("/", (req, res) => {
+      res.send("api is running");
+    });
+}
+  // ======================= deployment ============================
+  app.use(notFound);
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
